4d95e75333d5770a646d9231df1f6870ed6b2fa0
apoprtv <apoprtv@gmail.com>
2026-06-07 18:34:27 +0200

    update before refactor

--- analiza.py
+++ analiza.py
@@ -2,54 +2,64 @@
 # coding: utf-8
 
 import os
-import config
+from config import Config
 
 print("Starting FCS data processor...", flush=True)
 
-from file_processor import Processor
-
+from processor import Processor
 
 if __name__ == "__main__":
     print("Welcome to FCS data processor!", flush=True)
 
+    config = Config(
+        MIN_DIST=0,
+        XMAX=250000,
+        YMAX=250000,
+        BINS=1000,
+        SHOW_PLOTS=False,
+        SAVE_PLOTS=False,
+        VARIANT=0,
+    )
+
+    print("directory or filename: ", end="", flush=True)
+    directory = input()
+
+    print("minimal acceptable distance: ", end="", flush=True)
+    config.MIN_DIST = int(input() or 0)
+
+    print(
+        "Choose variant for cutting data points (0 for beneath the curve, 1 for above the curve, 2 for both): ",
+        end="",
+        flush=True,
+    )
+    variant = int(input() or 0)
+
+    print(
+        "max value for x axis (leave empty for default 250000): ",
+        end="",
+        flush=True,
+    )
+    config.XMAX = int(input() or 250000)
+
+    print(
+        "max value for y axis (leave empty for default 250000): ",
+        end="",
+        flush=True,
+    )
+    config.YMAX = int(input() or 250000)
+
+    print("Show plots? (1 for yes, 0 for no): ", end="", flush=True)
+    config.SHOW_PLOTS = bool(input() or 0)
+
+    print("Save plots? (1 for yes, 0 for no): ", end="", flush=True)
+    config.SAVE_PLOTS = bool(input() or 0)
+
     while True:
-
-        print("directory or filename: ", end="", flush=True)
-        directory = input()
-
-        print("minimal acceptable distance: ", end="", flush=True)
-        config.MIN_DIST = int(input() or 0)
-
-        print(
-            "Choose variant for cutting data points (0 for beneath the curve, 1 for above the curve, 2 for both): ",
-            end="",
-            flush=True,
-        )
-        variant = int(input() or 0)
-
-        print(
-            "max value for x axis (leave empty for default 250000): ",
-            end="",
-            flush=True,
-        )
-        config.XMAX = int(input() or 250000)
-
-        print(
-            "max value for y axis (leave empty for default 250000): ",
-            end="",
-            flush=True,
-        )
-        config.YMAX = int(input() or 250000)
-
-        print("Show plots? (1 for yes, 0 for no): ", end="", flush=True)
-        show_plots = bool(input() or 0)
-
         files = []
-
         processor = Processor()
 
         if directory[len(directory) - 3 :] == "fcs":
-            processor.process(directory, variant=variant, show_plots=show_plots)
+            result = processor.process(directory, config=config)
 
         elif directory == "" or not directory:
             files = os.listdir(os.getcwd())
@@ -60,8 +70,17 @@ if __name__ == "__main__":
         if files:
             for path in files:
                 if path.endswith(".fcs"):
-                    processor.process(path, variant=variant, show_plots=show_plots)
+                    result = processor.process(path, config=config)
                     print("Processing completed!", flush=True)
+                    if config.SHOW_PLOTS:
+                        print("Accept the filtered data? (1 for yes, 0 for no): ")
+                        user_accept = bool(int(input() or 0))
+                        if user_accept and config.SAVE_PLOTS:
+                            processor.save(result, config=config)
+                        else:
+                            print(
+                                "Data not saved. Moving to the next file.", flush=True
+                            )
 
         if not files or not any(file.endswith(".fcs") for file in files):
             print("No files found in the specified directory.", flush=True)
--- config.py
+++ config.py
@@ -1,4 +1,12 @@
-MIN_DIST = 0  # Minimum acceptable distance from the curve for a point to be included in the filtered data
-XMAX, YMAX = 250000, 250000  # Maximum values for x and y axes in the heatmap
-BINS = 1000  # Number of bins for the heatmap
-SHOW_PLOTS = False  # Whether to display plots during processing
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    MIN_DIST: int
+    XMAX: int
+    YMAX: int
+    BINS: int
+    SHOW_PLOTS: bool
+    SAVE_PLOTS: bool
+    VARIANT: int
